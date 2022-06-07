7e4ea11baaa8f084cfbd14bd5b1318c9082843bf
unknown <devadiga181@gmail.com>
2022-06-07 23:39:21 +0530

    Update Major

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 import pymongo
 import urllib
+import certifi
 
-conn_str = "mongodb+srv://admin:"+ urllib.parse.quote('admin')+"@cluster0.otftf.mongodb.net/?retryWrites=true&w=majority"
-client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
+conn_str = "mongodb+srv://admin:"+ urllib.parse.quote('admin')+"@cluster0.otftf.mongodb.net/?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000,tlsCAFile=certifi.where())
 db = client.db
 
 try:
